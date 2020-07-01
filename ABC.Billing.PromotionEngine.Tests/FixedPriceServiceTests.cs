de954aded22712a235252dde2f467c574cac4e87
SubhabrataGhosh1988 <subhabrata.ghosh.ms@gmail.com>
2020-07-02 00:11:28 +0530

    Writing the test cases for the fixed cost promotions

---
+++ ABC.Billing.PromotionEngine.Tests/FixedPriceServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using ABC.Billing.PromotionEngine.Services.Cart;
+using ABC.Billing.PromotionEngine.Services.Pricing;
+using ABC.Billing.PromotionEngine.Services.Promotions;
+using ABC.Billing.PromotionEngine.Services.Promotions.FixedPrice;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ABC.Billing.PromotionEngine.Tests
+{
+    /// <summary>
+    /// This is the test for Fixed Price service
+    /// </summary>
+    public class FixedPriceServiceTests
+    {
+        IPriceListService _priceListService;
+        ICartService _cartService;
+        /// <summary>
+        /// Test set up
+        /// </summary>
+        public FixedPriceServiceTests()
+        {
+            _priceListService = new PriceListService();
+            _cartService = new CartService(_priceListService);
+            _cartService.AddItem("A", 4);
+            _cartService.AddItem("B", 1);
+            _cartService.AddItem("C", 1);
+            _cartService.AddItem("D", 4);
+        }
+
+        /// <summary>
+        /// Test with out a cart set up.
+        /// </summary>
+        [Fact]
+        public void PromotionWithNoCartsTests()
+        {
+            IPromotionServices _promotionServices = new FixedPricePromotionServices(new List<string> { "C", "D" }, 30);
+            Assert.Equal(0, _promotionServices.Price());
+        }
+
+        /// <summary>
+        /// Test with out a price list set up
+        /// </summary>
+        [Fact]
+        public void PromotionWithNoPriceListsTests()
+        {
+            IPromotionServices _promotionServices = new FixedPricePromotionServices(new List<string> { "C", "D" }, 30);
+            _promotionServices.SetCart(_cartService);
+            Assert.Equal(0, _promotionServices.Price());
+        }
+        /// <summary>
+        /// Test with out a valid promotion set up
+        /// </summary>
+        [Fact]
+        public void PromotionWithInvalidItemsTests()
+        {
+            IPromotionServices _promotionServices = new FixedPricePromotionServices(new List<string> { "C", "D" }, 30);
+            _cartService.RemoveItem("C");
+            _promotionServices.SetCart(_cartService);
+            _promotionServices.SetPriceList(_priceListService);
+            Assert.Equal(_cartService.Price(), _promotionServices.Price());
+        }
+
+        /// <summary>
+        /// Test with a valid promotion set up..
+        /// </summary>
+        [Fact]
+        public void PromotionWithValidSkuItemsTests()
+        {
+            IPromotionServices _promotionServices = new FixedPricePromotionServices(new List<string> { "C", "D" }, 30);
+            _promotionServices.SetCart(_cartService);
+            _promotionServices.SetPriceList(_priceListService);
+            Assert.Equal(305, _promotionServices.Price());
+        }
+    }
+}
---
+++ ABC.Billing.PromotionEngine/Services/Promotions/FixedPrice/FixedPricePromotionServices.cs
@@ -0,0 +1,31 @@
+﻿using ABC.Billing.PromotionEngine.Services.Cart;
+using ABC.Billing.PromotionEngine.Services.Pricing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABC.Billing.PromotionEngine.Services.Promotions.FixedPrice
+{
+    public class FixedPricePromotionServices : IPromotionServices
+    {
+
+        public FixedPricePromotionServices(List<string> skuIds, decimal price)
+        {
+            throw new NotImplementedException();
+        }
+        public decimal Price()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCart(ICartService cartService)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPriceList(IPriceListService priceListService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
