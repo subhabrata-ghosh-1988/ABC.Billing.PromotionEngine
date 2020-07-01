28b07fbefe056fa1ccb9b0c5bae8bd2ce6206c11
SubhabrataGhosh1988 <subhabrata.ghosh.ms@gmail.com>
2020-07-01 22:39:35 +0530

    Add project files.

---
+++ ABC.Billing.PromotionEngine.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ABC.Billing.PromotionEngine.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ABC.Billing.PromotionEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC.Billing.PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
