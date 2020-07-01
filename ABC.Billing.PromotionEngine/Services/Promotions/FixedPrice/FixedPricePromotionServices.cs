9c5f96d85e9f45c3f91416dbf7cd3a2b06b091e7
SubhabrataGhosh1988 <subhabrata.ghosh.ms@gmail.com>
2020-07-02 00:19:47 +0530

    Implementing the promotional services and verify all the unit tests

--- ABC.Billing.PromotionEngine/Services/Promotions/FixedPrice/FixedPricePromotionServices.cs
+++ ABC.Billing.PromotionEngine/Services/Promotions/FixedPrice/FixedPricePromotionServices.cs
@@ -2,30 +2,76 @@
 using ABC.Billing.PromotionEngine.Services.Pricing;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ABC.Billing.PromotionEngine.Services.Promotions.FixedPrice
-{
+{/// <summary>
+/// Calculate price with Fixed promotions
+/// </summary>
     public class FixedPricePromotionServices : IPromotionServices
     {
+        private ICartService _cartService;
+        private IPriceListService _priceListService;
+
+        private List<string> _skuIds;
+        private decimal _price;
 
         public FixedPricePromotionServices(List<string> skuIds, decimal price)
         {
-            throw new NotImplementedException();
+            _skuIds = skuIds;
+            _price = price;
         }
+        /// <summary>
+        /// Calculate the price for the cart - with promotion code.
+        /// </summary>
+        /// <returns></returns>
         public decimal Price()
         {
-            throw new NotImplementedException();
-        }
+            if (_cartService != null && _priceListService != null)
+            {
+                var cartItems = _cartService.GetCartItems();
+                int minQty = Int32.MaxValue;
+                foreach (var _skuId in _skuIds)
+                {
+                    int qty = cartItems.Where(x => x.SkuId == _skuId).Select(r => r.Quantity).FirstOrDefault();
+                    if ( qty < minQty)
+                    {
+                        minQty = qty;
+                    }
+                }
+                if (minQty == 0)
+                {
+                    return _cartService.Price();
+                }
 
+                decimal fixed_price = minQty * _price;
+
+
+                decimal price = cartItems.Where(x => !_skuIds.Contains(x.SkuId)).Select(r => r.Quantity * _priceListService.GetPriceBySkuID(r.SkuId)).Sum()
+                                          + cartItems.Where(x => _skuIds.Contains(x.SkuId)).Select(r => (r.Quantity - minQty) * _priceListService.GetPriceBySkuID(r.SkuId)).Sum()
+                                          + fixed_price;
+
+
+                return price;
+            }
+            return 0;
+        }
+        /// <summary>
+        /// Set cart for discount.
+        /// </summary>
+        /// <param name="cartService"></param>
         public void SetCart(ICartService cartService)
         {
-            throw new NotImplementedException();
+            _cartService = cartService;
         }
-
+        /// <summary>
+        /// Price list service.
+        /// </summary>
+        /// <param name="priceListService"></param>
         public void SetPriceList(IPriceListService priceListService)
         {
-            throw new NotImplementedException();
+            _priceListService = priceListService;
         }
     }
 }
