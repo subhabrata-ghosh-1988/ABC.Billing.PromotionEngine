4633535c727cebb2df99ca9969253cc4e2bbb1fc
SubhabrataGhosh1988 <subhabrata.ghosh.ms@gmail.com>
2020-07-01 23:35:14 +0530

    Implementing the Price List Service

--- ABC.Billing.PromotionEngine/Services/Pricing/PriceListService.cs
+++ ABC.Billing.PromotionEngine/Services/Pricing/PriceListService.cs
@@ -1,19 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace ABC.Billing.PromotionEngine.Services.Pricing
 {
+    /// <summary>
+    /// This class reads the prices by SKU Ids from the configuration. 
+    /// </summary>
     public class PriceListService : IPriceListService
     {
-        public Dictionary<string, decimal> GetAllPrices()
+        private const string _filename = @"Data\PriceList.xml";
+        private Dictionary<string, decimal> _priceListings;
+        /// <summary>
+        /// Make a dictionary based on the configuration xml.
+        /// </summary>
+        public PriceListService()
         {
-            throw new NotImplementedException();
+            LoadPriceList();
         }
 
+        /// <summary>
+        /// This method Loads all the price list to the dictionary
+        /// </summary>
+        private void LoadPriceList()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var priceFilepath = Path.Combine(currentDirectory, _filename);
+            _priceListings = new Dictionary<string, decimal>();
+
+            XElement priceList = XElement.Load(priceFilepath);
+
+            _priceListings = (from item in priceList.Descendants("item")
+                              select new
+                              {
+                                  Name = Convert.ToString(item.Attribute("skuid").Value),
+                                  Value = Convert.ToDecimal(item.Attribute("price").Value)
+                              }).ToDictionary(x => x.Name, x => x.Value);
+        }
+
+        /// <summary>
+        /// Get price for a specific SKU ID. - Returns 0 for an invalid SKU ID.
+        /// </summary>
+        /// <param name="skuId">Provide a sku id</param>
+        /// <returns>Price</returns>
         public decimal GetPriceBySkuID(string skuId)
         {
-            throw new NotImplementedException();
+            if (!_priceListings.ContainsKey(skuId))
+            {
+                return 0;
+            }
+            return _priceListings[skuId];
+        }
+        /// <summary>
+        /// Get all price listing in the form of a dictionary
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, decimal> GetAllPrices()
+        {
+            return _priceListings;
         }
     }
 }
