387294a3b13c52408c87a5fed20ba907a709a225
SubhabrataGhosh1988 <subhabrata.ghosh.ms@gmail.com>
2020-07-01 22:43:51 +0530

    Initial solution setup - Main and Test project creation

--- ABC.Billing.PromotionEngine/Program.cs
+++ ABC.Billing.PromotionEngine/Program.cs
@@ -6,7 +6,7 @@ namespace ABC.Billing.PromotionEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
