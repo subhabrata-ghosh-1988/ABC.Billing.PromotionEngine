0860432c0d2133d7da7f6394640f537c9b5cbdae
SubhabrataGhosh1988 <subhabrata.ghosh.ms@gmail.com>
2020-07-01 23:22:11 +0530

    Adding configuration for the pricing list and the Model for the Cart Items.

---
+++ ABC.Billing.PromotionEngine/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABC.Billing.PromotionEngine.Models
+{
+    public class CartItem
+    {
+        /// <summary>
+        /// Sku ID of the item
+        /// </summary>
+        public string SkuId { get; set; }
+        /// <summary>
+        /// Total Quantity of the Cart Item.
+        /// </summary>
+        public int Quantity { get; set; }
+
+    }
+}
